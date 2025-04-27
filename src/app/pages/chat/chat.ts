425b861d58620eb317d04c190e83b42b55c723af
sheyla.yaker <sheyla.yaker@enarahealth.com>
2025-04-27 01:35:20 -0500

    refactor: integrate all changes and modify some part to match styles and content

--- src/app/pages/chat/chat.ts
+++ src/app/pages/chat/chat.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'chat',
-  imports: [],
-  templateUrl: './chat.html',
-  styleUrl: './chat.css',
+  templateUrl: 'chat.html',
+  styleUrl: 'chat.css',
 })
 export class Chat {}
